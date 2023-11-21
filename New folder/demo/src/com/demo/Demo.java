7ff5534cfcd57d96c824e6590f92ac0df67f84f6
Sanvi <chougalashridhar36@gmail.com>
2023-11-21 23:54:33 +0530

    commit the msg

---
+++ New folder/demo/src/com/demo/Demo.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+public class Demo {
+ public static void main(String[] args) {
+	 System.out.println(demo());
+ }
+ static int demo()
+ {
+	 protected int a=0;
+	 return ++a;
+ }
+}
+
---
+++ jdbc project practice/.metadata/.plugins/org.eclipse.core.resources/.history/f1/20c724318f4d001e19a2e3873fc56d7a
@@ -0,0 +1,9 @@
+package com.student.service;
+
+public interface Operation {
+	
+	public void addStudent();
+	public void getStudent();
+	public void updateStudent();
+	public void deleteStudent();
+}
